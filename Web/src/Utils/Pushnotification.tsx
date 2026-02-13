ca45d424c0182fd902d8fc51c874dfab10fbb599
MEHMET YILDIZ <yildiz655@gmail.com>
2026-02-13 15:24:03 +0300

    fix

--- Web/src/Utils/Pushnotification.tsx
+++ Web/src/Utils/Pushnotification.tsx
@@ -3,9 +3,8 @@ import React from 'react';
 import { store } from '@Api/store';
 import { gatewayApi } from '@Api/api';
 
-interface PushNotificationProps {
+export interface NotificationProps {
     Type: 'Success' | 'Information' | 'Error',
-    config?: ToastOptions,
     Subject: string,
     Description: string
 }
@@ -17,15 +16,22 @@ const CustomToast: React.FC<{ title: string, message: string }> = ({ title, mess
     </div>
 );
 
-const Pushnotification = ({ Subject, config, Description, Type }: PushNotificationProps) => {
+function Pushnotification(NotificationProps: NotificationProps[] | NotificationProps) {
+    let props = NotificationProps as NotificationProps[]
+    let notifications = Array.isArray(props) ? props : [props]
+
     const state = store.getState();
 
     const metaData = (gatewayApi.endpoints as any)?.getMeta.select()(state).data;
 
     const userConfig = metaData?.Config ? JSON.parse(metaData.Config) : {};
     const seperatedUserConfig = {
-        position: (userConfig?.Position ? `top-${userConfig.Position}` : 'top-right' ) as ToastOptions['position'],
+        position: (userConfig?.Position ? `top-${userConfig.Position}` : 'top-right') as ToastOptions['position'],
         autoClose: userConfig?.Duration ? parseInt(userConfig.Duration) : 5000,
+    }
+
+    let config: ToastOptions = {
+        closeOnClick: true,
     }
 
     const decoratedConfig: ToastOptions = {
@@ -33,17 +39,30 @@ const Pushnotification = ({ Subject, config, Description, Type }: PushNotificati
         ...(seperatedUserConfig)
     };
 
-    switch (Type) {
-        case "Success":
-            toast.success(<CustomToast title={Subject} message={Description} />, decoratedConfig);
-            break;
-        case "Information":
-            toast.info(<CustomToast title={Subject} message={Description} />, decoratedConfig);
-            break;
-        case "Error":
-            toast.error(<CustomToast title={Subject} message={Description} />, decoratedConfig);
-            break;
+    if (notifications && notifications.length > 0) {
+        notifications.forEach((notification) => {
+            if (notification) {
+                const { Type, Subject, Description } = notification
+                switch (Type) {
+                    case "Success":
+                        toast.success(<CustomToast title={Subject} message={Description} />, decoratedConfig);
+                        break;
+                    case "Information":
+                        toast.info(<CustomToast title={Subject} message={Description} />, decoratedConfig);
+                        break;
+                    case "Error":
+                        toast.error(<CustomToast title={Subject} message={Description} />, decoratedConfig);
+                        break;
+                    default:
+                        break;
+                }
+            } else {
+                toast.error(<CustomToast title={"Notification Error"} message={"Notification Object Can't Read"} />, config);
+            }
+        })
     }
+
+
 }
 
 export default Pushnotification;
